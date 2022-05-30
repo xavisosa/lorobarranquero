88c3a6c84322ac3583b8b53cf1216eea4f7344f0
Xavier Sosa <xavier.sosa2.contractor@gdevtools.com>
2022-05-30 18:31:00 -0300

    Se agregan las consultas del ejer5

---
+++ Ejercicios/ConsultasEjer5.sql
@@ -0,0 +1,33 @@
+-- 1
+-- Mostrar los artículos con precio mayor a 100
+
+-- 2
+-- Mostrar los artículos con precio entre 20 y 40 (usar < y >)
+
+-- 3
+-- Mostrar los artículos con precio entre 40 y 60 (usar BETWEEN)
+
+-- 4
+-- Mostrar los artículos con precio = 20 y stock mayor a 30
+
+-- 5
+-- Mostrar los artículos con precio (12,20,30) no usar IN
+
+-- 6
+-- Mostrar los artículos con precio (12,20,30) usar el IN
+
+-- 7
+-- Mostrar los artículos que su precio no sea (12,20,30)
+
+-- 8
+-- Listar los artículos ordenados por precio de mayor a menor, 
+-- y si hubiera precio iguales deben quedar ordenados por nombre.
+
+-- 9
+-- Listar todos los artículos incluyendo una columna denominada
+-- precio con IVA, la cual deberá tener el monto con el iva del producto.
+
+-- 10
+-- Listar todos los artículos incluyendo una columna denominada 
+-- “cantidad de cuotas” y otra “valor de cuota”, la cantidad es fija 
+-- y es 3, el valor de cuota corresponde a 1/3 del monto con un 5% de interés.
