a28dd690891820cf18b606057668fa082158b176
Xavier Sosa <xavier.sosa2.contractor@gdevtools.com>
2022-07-04 18:46:17 -0300

    Agrego función insertar

--- CineHoyts/funciones.py
+++ CineHoyts/funciones.py
@@ -29,3 +29,40 @@ def obtenerPeliculaPorId(cursor):
     cursor.execute(sql)
 
     return cursor.fetchall()
+
+def obtenerPeliculaPorAnio(cursor):
+    anio = int(input("Ingresa el año de la/s pelicula/s: "))
+
+    sql = f"SELECT * FROM peliculas WHERE anio = {anio}"
+    cursor.execute(sql)
+
+    return cursor.fetchall()
+
+def obtenerPeliculaPorDirector(cursor):
+    director = input("Ingresa el Director de la/s pelicula/s: ")
+
+    sql = f"SELECT * FROM peliculas WHERE direccion like '%{director}%'"
+    cursor.execute(sql)
+
+    return cursor.fetchall()
+
+# INSERT DE PELICULA
+def agregarPelicula(cursor):
+    idPelicula = int(input('Ingresa '))
+    titulo = input('Ingresa ')         
+    tituloOriginal = input('Ingresa ') 
+    anio = int(input('Ingresa '))           
+    certificado = input('Ingresa ')
+    duracion = input('Ingresa ')
+    genero = input('Ingresa ')
+    direccion = input('Ingresa ')
+    sinapsis = input('Ingresa ')
+    ISAM = input('Ingresa ')
+    puntuacionIMBd = float(input('Ingresa '))
+
+    sql = "INSERT INTO peliculas(idPelicula, titulo, tituloOriginal, anio, certificado, duracion, genero, direccion, sinapsis, isam, puntuacionIMBd)"
+    sql += "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    valores = (idPelicula, titulo, tituloOriginal, anio, certificado, duracion, genero, direccion, sinapsis, ISAM, puntuacionIMBd)
+
+    cursor.executemany(sql, valores)
+    print(f'{cursor.rowcount} pelicula guardada!')
