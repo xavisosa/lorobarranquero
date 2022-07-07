bc276542d349199a4512644032373cf4373c50c9
Xavier Sosa <xavier.sosa2.contractor@gdevtools.com>
2022-07-07 19:11:30 -0300

    1er ejercicio

--- ProblemasEjercicios/Ejercicio7-Python.py
+++ ProblemasEjercicios/Ejercicio7-Python.py
@@ -1,2 +1,13 @@
 # SITUACIONES PROBLEMATICAS
 
+def concatenarNombAp():
+
+    print('Esta función pide nombre y apellido por teclado e imprime el nombre completo')
+
+    _____ = input('Ingresa el Nombre: ')
+    apellido = ___________________________
+
+    return ________________________
+
+if __name__ == '__main__':
+    concatenarNombAp()
