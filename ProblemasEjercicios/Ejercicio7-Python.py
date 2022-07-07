c101034ab1aa82abe787804377ecee36db90402d
Xavier Sosa <xavier.sosa2.contractor@gdevtools.com>
2022-07-07 20:28:21 -0300

    2do ejercicio

--- ProblemasEjercicios/Ejercicio7-Python.py
+++ ProblemasEjercicios/Ejercicio7-Python.py
@@ -1,5 +1,6 @@
 # SITUACIONES PROBLEMATICAS
 
+# EJERCICIO 1
 def concatenarNombAp():
 
     print('Esta función pide nombre y apellido por teclado e imprime el nombre completo')
@@ -11,3 +12,29 @@ def concatenarNombAp():
 
 if __name__ == '__main__':
     print(concatenarNombAp())
+
+# EJERCICIO 2
+def calcularPorcentaje():
+
+    print('Función una recibe categoria y devuelve el porcetanje a aplicar')
+
+    _______ = input('Ingresar la categoria: ')
+
+    porc = 0
+
+    if _______ = 'A':
+        porc = 1.1
+    elif ______ = 'B':
+        porc = 1.2
+    _____:
+        porc = 1.05
+    
+    return _____________
+
+if __name__ == '__main__':
+    
+    precio = _____(input('Ingresar el monto: '))
+
+    precioDesc = precio __ ___________________________
+    
+    print(f'El precio con descuento es {precioDesc}')
